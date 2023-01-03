07e9e1a6e4e88a7e9938cde9f5f599867b632839
ggernigon <ggernigon@puydufou.com>
2023-01-03 13:51:50 +0100

    refacto appli

--- src/components/Menu/index.jsx
+++
@@ -1,9 +0,0 @@
-export default function Menu() {
-  const menuItem = ["Accueil", "Profil", "Réglage", "Communauté"];
-  const listMenu = menuItem.map((item) => <li>{item}</li>);
-  return (
-    <div className="menu">
-      <ul>{listMenu}</ul>
-    </div>
-  );
-}
