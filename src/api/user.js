13d7c117cbebcc2103eddbf786e058b31c06c351
ggernigon <guillaume.gernigon@outlook.fr>
2023-01-10 10:09:22 +0100

    add error handling

--- src/api/user.js
+++ src/api/user.js
@@ -2,19 +2,24 @@ import api from "./api";
 import User from "../models/User";
 
 export default async function getAllUsers() {
-  let userOne = await api.get("user/12");
-  userOne = new User(
-    userOne.data.data.id,
-    userOne.data.data.userInfos.firstName,
-    userOne.data.data.userInfos.lastName,
-    userOne.data.data.userInfos.age
-  );
-  let userTwo = await api.get("user/18");
-  userTwo = new User(
-    userTwo.data.data.id,
-    userTwo.data.data.userInfos.firstName,
-    userTwo.data.data.userInfos.lastName,
-    userTwo.data.data.userInfos.age
-  );
-  return [userOne, userTwo];
+  try {
+    let userOne = await api.get("user/12");
+    userOne = new User(
+      userOne.data.data.id,
+      userOne.data.data.userInfos.firstName,
+      userOne.data.data.userInfos.lastName,
+      userOne.data.data.userInfos.age
+    );
+    let userTwo = await api.get("user/18");
+    userTwo = new User(
+      userTwo.data.data.id,
+      userTwo.data.data.userInfos.firstName,
+      userTwo.data.data.userInfos.lastName,
+      userTwo.data.data.userInfos.age
+    );
+    return [userOne, userTwo];
+  } catch (error) {
+    console.log(error);
+    return error;
+  }
 }
--- src/routes/Home/Home.jsx
+++ src/routes/Home/Home.jsx
@@ -4,34 +4,34 @@ import { Link, useLoaderData } from "react-router-dom";
 export default function Home() {
   const { users } = useLoaderData();
 
-  return (
-    <div className="home">
-      <nav>
-        {users.length ? (
-          <ul>
-            {users.map((user) => (
-              <li key={user.id}>
-                <Link to={`/dashboard/${user.id}`}>
-                  {user.firstName || user.lastName ? (
-                    <UserButton
-                      firstName={user.firstName}
-                      lastName={user.lastName}
-                    />
-                  ) : (
-                    <>
-                      <UserButton firstName={"unknown"} lastName={"user"} />
-                    </>
-                  )}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        ) : (
-          <p>
-            <i>No User...</i>
-          </p>
-        )}
-      </nav>
-    </div>
-  );
+    return (
+      <div className="home">
+        <nav>
+          {users.length ? (
+            <ul>
+              {users.map((user) => (
+                <li key={user.id}>
+                  <Link to={`/dashboard/${user.id}`}>
+                    {user.firstName || user.lastName ? (
+                      <UserButton
+                        firstName={user.firstName}
+                        lastName={user.lastName}
+                      />
+                    ) : (
+                      <>
+                        <UserButton firstName={"unknown"} lastName={"user"} />
+                      </>
+                    )}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No User...</i>
+            </p>
+          )}
+        </nav>
+      </div>
+    );
 }
